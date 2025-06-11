fd253d48cc3df678e053171c5f303ad8d43e3892
Rohan Rakshit <116372469+rohanRakshit@users.noreply.github.com>
2025-06-11 13:23:33 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kyzerrak@gmail.com"
-EMAIL_HOST_PASSWORD = "dxpr pbod tuvr hakn"
+EMAIL_HOST_USER = "Your email id"
+EMAIL_HOST_PASSWORD = "Your app password"
